267b509d8d55cd2390a9a34587cc7b75ccf788ca
Ayush Tripathi <62866081+AyushTrip@users.noreply.github.com>
2021-04-25 19:54:19 -0500

    Update baseball.cpp

--- baseball/baseball.cpp
+++ baseball/baseball.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <fstream>
 
+using namespace std;
+
+int N;
+
+int main(){
+  ifstream fin("baseball.in");
+  ofstream fout("baseball.out");
+
+  fin >> N;
+  int cows[N];
+  for (int i=0; i<N; i++){
+    fin >> cows[i];
+  }
+  fin.close();
+
+  sort(cows, cows + N);
+
+  int answer = 0;
+  for(int i=0; i<N; i++){
+    for (int j=i+1; j<N; j++){
+      for (int k=j+1; k<N; k++){
+        int first_throw = cows[j] - cows[i];
+        int second_throw = cows[k] - cows[j];
+
+        if (first_throw <= second_throw && second_throw <= 2*first_throw){
+          answer++;
+        }
+      }
+    }
+  }
+  fout << answer;
+  fout.close();
+  return 0;
+}
