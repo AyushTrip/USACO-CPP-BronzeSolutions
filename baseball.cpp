d926e254c11c2b00bfca08e2dbb7a1c145fa052c
Ayush Tripathi <62866081+AyushTrip@users.noreply.github.com>
2021-04-25 19:52:35 -0500

    Add files via upload

---
+++ baseball.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+//C:/Users/ayush/OneDrive/Desktop/USACO/bronze/A-L/baseball
+int N;
+
+int main(){
+  ifstream fin("baseball.in");
+  ofstream fout("baseball.out");
+
+  fin >> N;
+  int cows[N];
+  for (int i=0; i<N; i++){
+    fin >> cows[i];
+  }
+  fin.close();
+
+  sort(cows, cows + N);
+
+  int answer = 0;
+  for(int i=0; i<N; i++){
+    for (int j=i+1; j<N; j++){
+      for (int k=j+1; k<N; k++){
+        int first_throw = cows[j] - cows[i];
+        int second_throw = cows[k] - cows[j];
+        //cout << cows[i] << " " << cows[j] << " " << cows[k] << " first " << first_throw << " second " << second_throw << endl;
+
+        if (first_throw <= second_throw && second_throw <= 2*first_throw){
+          answer++;
+        }
+      }
+    }
+  }
+  fout << answer;
+  fout.close();
+  return 0;
+}
